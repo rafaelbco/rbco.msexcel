e149f8c37f3dec3d2b6a8dbab98a1946ca7974c9
Rafael Oliveira <rafaelbco@gmail.com>
2014-01-29 12:45:41 +0000

    Corrected bug in rows_and_columns_to_matrix
    git-svn-id: svn://localhost/tmp/svn2git/svn_repo/trunk@6 10b106a8-13b5-4a0f-ae25-c5a0c82a28fc

--- rbco/msexcel/__init__.py
+++ rbco/msexcel/__init__.py
@@ -8,55 +8,58 @@ def xls_to_excelerator_dict(filename, encoding=None):
 def fix_value(v):
     if isinstance(v, unicode):
         return v.encode('utf8').strip()
-        
-    return v    
-       
+
+    return v
+
 def print_excelerator_dict(excelerator_dict):
     for sheet_name, data in excelerator_dict:
         print 'Sheet: %s' % fix_value(sheet_name)
         for k, v in sorted(data.items()):
             print k, fix_value(v)
-    
+
 def sequence_with_index(seq):
     return zip(range(len(seq)), seq)
-        
+
 def print_matrix(matrix):
     for sheet, rows in matrix.iteritems():
         print 'Sheet: %s' % sheet
-        
+
         for (row_num, columns) in sequence_with_index(rows):
             print 'row: %d' % row_num
             for (col_num, v) in sequence_with_index(columns):
                 print col_num, v
             print
-    
+
 def excelerator_dict_to_rows_and_columns(excelerator_dict):
     sheets = {}
-    
+
     for sheet, data in excelerator_dict:
         rows = {}
         for ((row_num, col_num), v) in data.iteritems():
             rows.setdefault(row_num, {})[col_num] = fix_value(v)
-        
+
         sheets[sheet] = rows
-    
+
     return sheets
 
 def rows_and_columns_to_matrix(rows_and_columns):
-    def get_all_in_range(dict):
-        return [dict.get(i) for i in xrange(max(dict.keys()))]
-    
     new_sheets = {}
-    for (sheet, rows) in rows_and_columns.iteritems():
-        rows = [get_all_in_range(cols) for cols in get_all_in_range(rows)]
-        new_sheets[sheet] = rows    
-    
-    return new_sheets
+    for (sheet, d) in rows_and_columns.iteritems():
+        new_sheet = []
+        row_numbers = sorted(d.keys())
+        for row_number in row_numbers:
+            row = d[row_number]
+            new_row = [row[i] for i in sorted(row.keys())]
 
+            new_sheet.append(new_row)
+
+        new_sheets[sheet] = new_sheet
+
+    return new_sheets
 
 def sequence_to_dict(seq, index_names):
     return dict(zip(index_names, seq))
-        
+
 def matrix_to_structure(matrix):
     return dict(
         (sheet, [sequence_to_dict(cols, rows[0]) for cols in rows[1:]])
@@ -73,7 +76,7 @@ def print_structure(dicts):
         for d in data:
             print_dict(d)
             print
-                
+
 def xls_to_structure(path):
     return matrix_to_structure(
         rows_and_columns_to_matrix(
@@ -83,4 +86,4 @@ def xls_to_structure(path):
         )
     )
 
-    
+
